1026aa8dad63c33dfe6780be20397fbeda60e174
pablodaniel28 <castedo.pablo@ficct.uagrm.edu.bo>
2025-11-12 08:31:57 -0400

    actualizar

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import app from "./app.js";
 import { connectDB, sequelize } from "./config/db.js";
 
-dotenv.config();
+// dotenv.config();
 const PORT = process.env.PORT || 4000;
 
 const startServer = async () => {
